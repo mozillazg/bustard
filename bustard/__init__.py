feb07a2c25ff1ba33ad40e5408300629901d70f8
mozillazg <opensource.mozillazg@gmail.com>
2016-02-19 19:48:08 +0800

    update setup.py

--- bustard/__init__.py
+++ bustard/__init__.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = '0.0.1'
+'''A tiny WSGI web framework'''
+
+__version__ = '0.1.0'
 __title__ = 'bustard'
 __author__ = 'mozillazg'
 __license__ = 'MIT'
-__copyright__ = 'Copyright (c) 2015 mozillazg'
+__copyright__ = 'Copyright (c) 2016 mozillazg'
--- setup.py
+++ setup.py
@@ -14,29 +14,29 @@ except ImportError:
 import bustard
 
 if sys.argv[-1] == 'publish':
+    os.system('python setup.py register')
     os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
+    os.system('python setup.py bdist_wheel upload --universal')
     sys.exit()
 
 packages = [
     'bustard',
 ]
 
-requirements = []
-if sys.version_info[:2] < (2, 7):
-    requirements.append('argparse')
+requirements = [
+    'psycopg2',
+]
 
 
 def long_description():
     readme = open('README.rst', encoding='utf8').read()
-    text = readme + '\n\n' + open('CHANGELOG.rst', encoding='utf8').read()
-    return text
+    return readme
 
 setup(
     name=bustard.__title__,
     version=bustard.__version__,
     description=bustard.__doc__,
-    # long_description=long_description(),
+    long_description=long_description(),
     url='https://github.com/mozillazg/bustard',
     author=bustard.__author__,
     author_email='mozillazg101@gmail.com',
@@ -47,4 +47,17 @@ setup(
     include_package_data=True,
     install_requires=requirements,
     zip_safe=False,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
 )
